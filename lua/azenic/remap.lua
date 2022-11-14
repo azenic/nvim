25c96c62ef888b93360ca2b354bff8a6f1e02b16
ivanazenic <ivan.azenic@cobeisfresh.com>
2022-11-14 10:05:16 +0100

    added show docummentation in preview

--- lua/azenic/remap.lua
+++ lua/azenic/remap.lua
@@ -1,7 +1,6 @@
 local nnoremap = require("azenic.keymap").nnoremap
 nnoremap ("<leader>fe", "<cmd>CocCommand flutter.emulators<CR>")
 nnoremap ("<leader>ff", "<cmd>FZF<CR>")
-
 --- Coc binds
 --- Use <c-space> to trigger completion.
 local keyset = vim.keymap.set
@@ -10,6 +9,20 @@ function _G.check_back_space()
     local col = vim.fn.col('.') - 1
     return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') ~= nil
 end
+
+-- Use K to show documentation in preview window.
+function _G.show_docs()
+    local cw = vim.fn.expand('<cword>')
+    if vim.fn.index({'vim', 'help'}, vim.bo.filetype) >= 0 then
+        vim.api.nvim_command('h ' .. cw)
+    elseif vim.api.nvim_eval('coc#rpc#ready()') then
+        vim.fn.CocActionAsync('doHover')
+    else
+        vim.api.nvim_command('!' .. vim.o.keywordprg .. ' ' .. cw)
+    end
+end
+keyset("n", "K", '<CMD>lua _G.show_docs()<CR>', {silent = true})
+
 keyset("i", "<c-space>", "coc#refresh()", {silent = true, expr = true})
 keyset("n", "gd", "<Plug>(coc-definition)", {silent = true})
 keyset("n", "gy", "<Plug>(coc-type-definition)", {silent = true})
