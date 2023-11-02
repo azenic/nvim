9c50f9e80885c04200c1baad5836ffa022bc3f1c
Ivan Azenic <azenicivan@gmail.com>
2023-11-02 12:42:05 +0100

    Delete lua/azenic directory

--- lua/azenic/init.lua
+++
@@ -1,8 +0,0 @@
-require("azenic.set")
-require("azenic.remap")
-require("azenic.vim-plug")
-require("azenic.nvim-tree")
-require("azenic.feline")
-require("azenic.nvim-web-devicons")
-require("azenic.gitsigns")
-require("azenic.treesitter")
